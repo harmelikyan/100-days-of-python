e98f771c16d4a1327b09739f65720fa92fe7cb91
Harutyun Melikyan <65981737+harmelikyan@users.noreply.github.com>
2021-12-03 06:32:26 +0000

    initial

---
+++ day14/main.py
@@ -0,0 +1,14 @@
+# Import random module
+import random
+
+# import logo and vs from art.py 
+from art import logo, vs
+
+
+#import data from game_data
+from game_data import data
+
+
+# create function where logo and vs will be printed, where two people will be randomly generated from the list compared together. User will be asked which one has higher follower number.
+# if the user's answer is correct the score will be incremented, if not then the game will start over.
+
