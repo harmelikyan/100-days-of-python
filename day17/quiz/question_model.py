29d2adce6dcc927162257c6fa3c06424015416f2
harmelikyan <65981737+harmelikyan@users.noreply.github.com>
2021-12-13 17:40:38 +0000

    Add files via upload

---
+++ day17/quiz/question_model.py
@@ -0,0 +1,10 @@
+# TODO: Create a Question class with an __init__()__ method with two attributes: text and answer attribute
+
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+
