a21ccdff7de4d6c10c9dc6314d37ec5d53d03d9b
harmelikyan <65981737+harmelikyan@users.noreply.github.com>
2021-12-24 22:43:01 +0000

    Add files via upload

--- day18/hirst painting/main.py
+++ day18/hirst painting/main.py
@@ -1,1 +1,30 @@
+import turtle
+import turtle as turtle_module
+import random
 
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+color_list = [(218, 223, 230), (230, 207, 91), (225, 149, 91), (122, 167, 187), (35, 110, 158), (163, 13, 22), (127, 177, 145), (233, 81, 49), (202, 67, 27), (192, 186, 23), (174, 18, 13), (33, 129, 49), (7, 99, 37), (13, 64, 39), (12, 41, 76), (242, 203, 4), (139, 79, 92), (88, 13, 20), (53, 166, 76), (51, 23, 19), (180, 134, 146), (6, 64, 137), (213, 68, 75), (230, 170, 161), (49, 151, 191), (77, 133, 186), (175, 204, 176)]
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 101
+
+
+for dot_count in range(1, number_of_dots):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen = turtle_module.Screen()
+screen.exitonclick()
