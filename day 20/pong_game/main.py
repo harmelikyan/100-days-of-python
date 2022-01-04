4c2232216504031c0fe6985203eedefc695d321f
harmelikyan <65981737+harmelikyan@users.noreply.github.com>
2022-01-04 09:40:29 +0000

    Add files via upload

--- day 20/pong_game/main.py
+++ day 20/pong_game/main.py
@@ -1,49 +1,56 @@
-# Import Modules
-from turtle import Screen
-from snake import Snake
-from food import Food
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
 from scoreboard import Scoreboard
 import time
 
-# Screen functions
+
 screen = Screen()
-screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("The Snake")
+screen.setup(width=800, height=600)
+screen.title("Pong")
 screen.tracer(0)
 
-# Calling the snake class from snake.py
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
 
-# Game logic to update the screen, so that, the snake can move
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
-    time.sleep(0.1)
-    snake.move()
+    ball.move()
 
-    # Detect collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
+    # Detect collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
-    # Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.reset()
-        snake.reset()
-    # Detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            scoreboard.reset()
-            snake.reset()
+    # Detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        ball.bounce_x()
+        scoreboard.l_point()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        ball.bounce_x()
+        scoreboard.r_point()
+
+
+
 
 screen.exitonclick()
-
