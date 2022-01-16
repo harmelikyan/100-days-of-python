947868ae42eeee20ea600e29c687590a834588f8
harmelikyan <harmelikyan@gmail.com>
2022-01-16 21:19:31 +0000

    modified day 29

--- day 29/password-manager-start/main.py
+++ day 29/password-manager-start/main.py
@@ -11,10 +11,9 @@ def generate_password():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-    password_numbers = [choice(letters) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
     password_list = password_letters + password_symbols + password_numbers
     shuffle(password_list)
