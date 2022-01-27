35355635a0ad19d3b66c1bcdb63339b8c9cf2c9b
harmelikyan92 <harut@playbrush.io>
2022-01-27 22:20:15 +0000

    day 32

---
+++ day 32/birthday-wisher-extrahard-start/main.py
@@ -0,0 +1,31 @@
+# ##################### Extra Hard Starting Project ######################
+import pandas as pd
+from datetime import datetime
+import smtplib
+import random
+
+MY_EMAIL = "tt2689933@gmail.com"
+PASSWORD = "99714171h"
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pd.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+        print(contents)
+
+
+# 4. Send the letter generated in step 3 to that person's email address.
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=birthday_person["email"],
+                            msg=f"Subject:Happy Birthday\n\n{contents}")
+
+
